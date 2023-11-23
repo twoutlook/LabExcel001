38f533f0df8b472ee72f02af67ac97cdc3ea1f32
Mark <caotunspring@outlook.com>
2023-11-23 08:47:08 +0800

    Add project files.

---
+++ BlazorApp1/Data/ExcelController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+
+namespace BlazorApp1.Data
+{
+    [ApiController]
+    [Route("[controller]")] // This will set the route to /excel
+    public class ExcelController:ControllerBase
+    {
+        [HttpGet("GetExcel")]
+        public async Task<IActionResult> GetExcel()
+        {
+            var stream = new MemoryStream();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(stream))
+            {
+                var workSheet = package.Workbook.Worksheets.Add("Sheet1");
+                workSheet.Cells.LoadFromCollection(new List<ExcelModel>
+                {
+                    new ExcelModel { Id = 1, Name = "John", Surname = "Doe" },
+                    new ExcelModel { Id = 2, Name = "Jane", Surname = "Doe" },
+                    new ExcelModel { Id = 3, Name = "John", Surname = "Smith" }
+                }, true);
+                package.Save();
+            }
+            stream.Position = 0;
+            string excelName = $"Excel_{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.xlsx";
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
+        }   
+    }
+}
---
+++ BlazorApp1/Data/ExcelModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Data
+{
+    internal class ExcelModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
